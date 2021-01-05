f4d83dc09eaccf0fa1b4cfbbb3b28d38fbfe9fc5
Phillip Castillo <PhillipACastillo@gmail.com>
2021-01-05 09:16:30 -0800

    Connect confirmation buttons to Address and CardDetailScreens

--- client/src/screens/ConfirmationScreen.js
+++ client/src/screens/ConfirmationScreen.js
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react';
-import { StyleSheet, FlatList } from 'react-native';
-import { Text, Button, ListItem } from 'react-native-elements';
+import { StyleSheet, FlatList, ScrollView} from 'react-native';
+import { Card, Text, Button, ListItem, Divider } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import {
   Context as AppointmentContext
 } from '../context/AppointmentContext';
 
-const ConfirmationScreen = () => {
-  const { state: { dateTime, cardInfo, addresses } } = useContext(AppointmentContext)
+const ConfirmationScreen = ({ navigation }) => {
+  const { state: { appointmentReason, dateTime, cardInfo, addresses } } = useContext(AppointmentContext)
 
   const day = new Date(dateTime).toLocaleDateString('en-US', {
     day: 'numeric',
@@ -21,11 +21,31 @@ const ConfirmationScreen = () => {
   });
 
   return (
-    <>
-      <Text h3> Appointment Date</Text>
-      <Text>{day} at {time}</Text>
+    <ScrollView
+      showsVerticalScrollIndicator ={false}
+      showsHorizontalScrollIndicator={false}
+    >
+      <Text h1 style={{ textAlign: 'center'}}>
+        Go With The Flow Plumbing
+      </Text>
+      <Divider />
+      <Card>
+        <Card.Title style={{fontSize: 16}}>Your Issue</Card.Title>
+        <Card.Divider />
+        <Text>
+          Skate ipsum dolor sit amet, crail grab Slimeballs tailslide camel back gap bigspin full-cab hip. Coffin fastplant Tracker tuna-flip late fakie out concave. Trucks bone air no comply hang-up masonite air layback. Rail slide shinner gap frigid air stalefish kick-nose slide. Rails street boneless sketchy bigspin salad grind indy grab. Deck fastplant half-flip carve 1080 late layback Gator Mark Anthony. Varial ollie ollie hole chicken wing baseplate freestyle hang ten.
+          {/* {appointmentReason} */}
+        </Text>
+      </Card>
       <Spacer>
-        <Text h3>Payment Information</Text>
+        <Text h3 style={styles.center}> Appointment Date</Text>
+      </Spacer>
+      <Spacer>
+      <Text style={styles.center}>{day} at {time}</Text>
+      </Spacer>
+      <Divider />
+      <Spacer>
+        <Text h3 style={styles.center}>Payment Information</Text>
         <FlatList
           data={cardInfo}
           keyExtractor={item => item.number}
@@ -39,10 +59,12 @@ const ConfirmationScreen = () => {
         />
         <Button
           title="Add a New Card"
+          onPress={() => navigation.navigate('Card')}
         />
       </Spacer>
+      <Divider />
       <Spacer>
-        <Text h3>Addresses</Text>
+        <Text h3 style={styles.center}>Addresses</Text>
         <FlatList
           data={addresses}
           keyExtractor={item => item.line1}
@@ -60,13 +82,21 @@ const ConfirmationScreen = () => {
         />
         <Button
           title="Add a New Address"
+          onPress={() => navigation.navigate('Address')}
         />
       </Spacer>
+      <Divider />
+      <Spacer>
       <Button title="Confirm" />
-    </>
+      </Spacer>
+    </ScrollView>
   )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  center: {
+    textAlign: 'center',
+  },
+});
 
 export default ConfirmationScreen;
