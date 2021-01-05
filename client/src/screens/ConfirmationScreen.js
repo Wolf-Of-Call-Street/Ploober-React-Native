1080903686c059e801f9ce94ed51f2a049335f6a
Phillip Castillo <PhillipACastillo@gmail.com>
2021-01-04 22:45:12 -0800

    Transform utc-epoch time to human-readable time

--- client/src/screens/ConfirmationScreen.js
+++ client/src/screens/ConfirmationScreen.js
@@ -1,48 +1,68 @@
 import React, { useContext } from 'react';
 import { StyleSheet, FlatList } from 'react-native';
 import { Text, Button, ListItem } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 import {
   Context as AppointmentContext
 } from '../context/AppointmentContext';
 
 const ConfirmationScreen = () => {
-  const { state: { cardInfo, addresses } } = useContext(AppointmentContext)
+  const { state: { dateTime, cardInfo, addresses } } = useContext(AppointmentContext)
+
+  const day = new Date(dateTime).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
+  const time = new Date(dateTime).toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    minute: '2-digit',
+  });
+
   return (
     <>
-      <Text h3>Payment Information</Text>
-      <FlatList
-        data={cardInfo}
-        keyExtractor={item => item.number}
-        renderItem={({ item }) => {
-          <ListItem bottomDivider>
-            <ListItem.Title>
-              Card Number Ending in {item.number.slice(-4)}
-            </ListItem.Title>
-          </ListItem>
-        }}
-      />
-      <Button
-        title="Add a New Card"
-      />
-      <Text h3>Addresses</Text>
-      <FlatList
-        data={addresses}
-        keyExtractor={item => item.line1}
-        renderItem={({ item }) => {
-          <ListItem bottomDivider>
-            <ListItem.Title>
-              {item.line1}
-              {item.line2}
-              {item.zipcode}
-              {item.state}
-              {item.city}
-            </ListItem.Title>
-          </ListItem>
-        }}
-      />
-      <Button
-        title="Add a New Address"
-      />
+      <Text h3> Appointment Date</Text>
+      <Text>{day} at {time}</Text>
+      <Spacer>
+        <Text h3>Payment Information</Text>
+        <FlatList
+          data={cardInfo}
+          keyExtractor={item => item.number}
+          renderItem={({ item }) => {
+            <ListItem bottomDivider>
+              <ListItem.Title>
+                Card Number Ending in {item.number.slice(-4)}
+              </ListItem.Title>
+            </ListItem>
+          }}
+        />
+        <Button
+          title="Add a New Card"
+        />
+      </Spacer>
+      <Spacer>
+        <Text h3>Addresses</Text>
+        <FlatList
+          data={addresses}
+          keyExtractor={item => item.line1}
+          renderItem={({ item }) => {
+            <ListItem bottomDivider>
+              <ListItem.Title>
+                {item.line1}
+                {item.line2}
+                {item.zipcode}
+                {item.state}
+                {item.city}
+              </ListItem.Title>
+            </ListItem>
+          }}
+        />
+        <Button
+          title="Add a New Address"
+        />
+      </Spacer>
+      <Button title="Confirm" />
     </>
   )
 };
