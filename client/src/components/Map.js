2044ad7a19b013a85de569a064b345fc8ae31d39
Jimmy Lin <hijimmy88@gmail.com>
2021-01-04 17:26:26 -0800

    Created component for map and plumbers.

---
+++ client/src/components/Map.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import MapView from 'react-native-maps';
+import { Text } from 'react-native-elements';
+import { StyleSheet, View } from 'react-native';
+
+const Map = () => {
+
+  return (
+    <View>
+      <Text h3>I am map!!</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({});
+
+export default Map;
