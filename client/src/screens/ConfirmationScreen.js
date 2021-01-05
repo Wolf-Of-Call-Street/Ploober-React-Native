3a2247ef726da9cdf2a3a2dd0b690d66d22c2003
Phillip Castillo <PhillipACastillo@gmail.com>
2021-01-04 22:14:44 -0800

    Add list components for payment and address information

--- client/src/screens/ConfirmationScreen.js
+++ client/src/screens/ConfirmationScreen.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
+import React, { useContext } from 'react';
+import { StyleSheet, FlatList } from 'react-native';
+import { Text, Button, ListItem } from 'react-native-elements';
+import {
+  Context as AppointmentContext
+} from '../context/AppointmentContext';
 
 const ConfirmationScreen = () => {
+  const { state: { cardInfo, addresses } } = useContext(AppointmentContext)
   return (
-    <View>
-      <Text>This is the confirmation screen</Text>
-    </View>
+    <>
+      <Text h3>Payment Information</Text>
+      <FlatList
+        data={cardInfo}
+        keyExtractor={item => item.number}
+        renderItem={({ item }) => {
+          <ListItem bottomDivider>
+            <ListItem.Title>
+              Card Number Ending in {item.number.slice(-4)}
+            </ListItem.Title>
+          </ListItem>
+        }}
+      />
+      <Button
+        title="Add a New Card"
+      />
+      <Text h3>Addresses</Text>
+      <FlatList
+        data={addresses}
+        keyExtractor={item => item.line1}
+        renderItem={({ item }) => {
+          <ListItem bottomDivider>
+            <ListItem.Title>
+              {item.line1}
+              {item.line2}
+              {item.zipcode}
+              {item.state}
+              {item.city}
+            </ListItem.Title>
+          </ListItem>
+        }}
+      />
+      <Button
+        title="Add a New Address"
+      />
+    </>
   )
 };
 
