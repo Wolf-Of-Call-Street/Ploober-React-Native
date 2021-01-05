9c0235fb94a99e3014a331687699b025ad62c4c6
Jimmy Lin <hijimmy88@gmail.com>
2021-01-05 13:42:07 -0800

    User current location now syncs with MapView. Will now ask to allow for location services.

--- client/src/screens/MapScreen.js
+++ client/src/screens/MapScreen.js
@@ -1,20 +1,64 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Map from '../components/Map';
 import Plumber from '../components/Plumber';
 import {Context as AppointmentContext} from '../context/AppointmentContext';
+import { requestPermissionsAsync } from 'expo-location';
+import Spacer from '../components/Spacer';
 
 const MapScreen = () => {
-  const {getLocalBusiness} = useContext(Context);
+  const [err, setErr] = useState(null);
+  const [lat, setLat] = useState(0);
+  const [long, setLong] = useState(0);
+  console.log(lat, long, 'inside MapScreen');
+  const {getLocalBusiness} = useContext(AppointmentContext);
+
+  const startLocationTracking = async (next) => {
+    try {
+      const {granted} = await requestPermissionsAsync();
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
+      setErr(null);
+      next();
+    } catch (err) {
+      setErr(err);
+    }
+  };
+
+
+  const getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    })
+  };
+  useEffect( () => {
+     startLocationTracking(getUserLocation);
+  }, []);
+
 
   return (
-    <View>
-      <Map />
+    <View style={styles.container}>
+      <Map currentLocation={{ lat, long }}/>
+      {err ? <Text style={styles.locationFail}>Please enable location services!</Text> : null}
+      <Spacer />
       <Plumber />
     </View>
   )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 10,
+    borderColor: 'green',
+    borderWidth: 1
+  },
+  locationFail: {
+    color: 'red',
+    fontSize: 18
+  }
+});
 
 export default MapScreen;
