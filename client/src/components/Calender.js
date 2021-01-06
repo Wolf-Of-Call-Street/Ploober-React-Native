bc8dad35ec3c75bf9ca772db30ed0536a5230e8d
Bramble <janem.draws@gmail.com>
2021-01-06 10:44:12 -0700

    Month/Year calendar implemented. Issue box created.

--- client/src/components/Calender.js
+++ client/src/components/Calender.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+
+
 
 const Calender = () => {
   return (
     <View>
-      <Text> Calender!!! </Text>
+      <CalendarList
+        minDate={Date()}
+        onDayPress={(day) => {
+
+        }}
+        monthFormat={'MMMM yyyy'}
+        pastScrollRange={0}
+        futureScrollRange={6}
+        horizontal={true}
+        pagingEnabled={true}
+        hideExtraDays={true}
+
+      />
     </View>
   )
 };
--- client/src/components/Issue.js
+++ client/src/components/Issue.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text } from 'react-native';
+import { Input } from 'react-native-elements';
 
-const Issue = () => {
+const Issue = ({onSubmit}) => {
+  const [issue, setIssue] = useState('');
+
   return (
     <View>
-      <Text> Issues here!!!! </Text>
+      <Text> Please explain your issue below: </Text>
+      <Input
+        label="Issue"
+        value={issue}
+        onChangeText={setIssue}
+        autoCorrect={false}
+      />
     </View>
   )
 };
