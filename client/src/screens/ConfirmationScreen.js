c36e32a4a37707d5ac7dfd11de2f07a6563f9d9a
Phillip Castillo <PhillipACastillo@gmail.com>
2021-01-07 06:01:02 -0800

    Render addresses as a FlatList

--- client/src/screens/ConfirmationScreen.js
+++ client/src/screens/ConfirmationScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { StyleSheet, FlatList, ScrollView} from 'react-native';
+import { StyleSheet, FlatList, ScrollView, View, TouchableOpacity } from 'react-native';
 import { Card, Text, Button, ListItem, Divider } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import {
@@ -8,7 +8,7 @@ import {
 import { NavigationEvents } from 'react-navigation';
 
 const ConfirmationScreen = ({ navigation }) => {
-  const { state: { appointmentReason, dateTime, addresses}, fetchAddresses, state } = useContext(AppointmentContext);
+  const { state: { appointmentReason, dateTime, addresses }, fetchAddresses, state } = useContext(AppointmentContext);
 
   const day = new Date(dateTime).toLocaleDateString('en-US', {
     day: 'numeric',
@@ -23,56 +23,79 @@ const ConfirmationScreen = ({ navigation }) => {
 
   return (
     <>
-    <NavigationEvents
-      onWillFocus={fetchAddresses}
-    />
-    <ScrollView
-      showsVerticalScrollIndicator ={false}
-      showsHorizontalScrollIndicator={false}
-      nestedScrollEnabled
-    >
-      <Text h1 style={{ textAlign: 'center'}}>
-        Go With The Flow Plumbing
+      <NavigationEvents
+        onWillFocus={fetchAddresses}
+      />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+        nestedScrollEnabled
+      >
+        <Text h1 style={{ textAlign: 'center' }}>
+          Go With The Flow Plumbing
       </Text>
-      <Divider />
-      <Card>
-        <Card.Title style={{fontSize: 16}}>Your Issue</Card.Title>
-        <Card.Divider />
-        <Text>
-          Skate ipsum dolor sit amet, crail grab Slimeballs tailslide camel back gap bigspin full-cab hip. Coffin fastplant Tracker tuna-flip late fakie out concave. Trucks bone air no comply hang-up masonite air layback. Rail slide shinner gap frigid air stalefish kick-nose slide. Rails street boneless sketchy bigspin salad grind indy grab. Deck fastplant half-flip carve 1080 late layback Gator Mark Anthony. Varial ollie ollie hole chicken wing baseplate freestyle hang ten.
-          {/* {appointmentReason} */}
+        <Divider />
+        <Card>
+          <Card.Title style={{ fontSize: 16 }}>Your Issue</Card.Title>
+          <Card.Divider />
+          <Text>
+            Skate ipsum dolor sit amet, crail grab Slimeballs tailslide camel back gap bigspin full-cab hip. Coffin fastplant Tracker tuna-flip late fakie out concave. Trucks bone air no comply hang-up masonite air layback. Rail slide shinner gap frigid air stalefish kick-nose slide. Rails street boneless sketchy bigspin salad grind indy grab. Deck fastplant half-flip carve 1080 late layback Gator Mark Anthony. Varial ollie ollie hole chicken wing baseplate freestyle hang ten.
         </Text>
-      </Card>
-      <Spacer>
-        <Text h3 style={styles.center}> Appointment Date</Text>
-      </Spacer>
-      <Spacer>
-      <Text style={styles.center}>{day} at {time}</Text>
-      </Spacer>
-      <Divider />
-      <Spacer>
-        <Text h3 style={styles.center}>Payment Information</Text>
-        <Button
-          title="Add a New Card"
-          onPress={() => navigation.navigate('Card')}
-        />
-      </Spacer>
-      <Divider />
-      <Spacer>
-        <Text h3 style={styles.center}>Addresses</Text>
-        <Text>{addresses.length} adresses in memory</Text>
-        <Button
-          title="Add a New Address"
-          onPress={() => {
-            navigation.navigate('Address')
-          }}
-        />
-      </Spacer>
-      <Divider />
-      <Spacer>
-      <Button title="Confirm" />
-      </Spacer>
-    </ScrollView>
+        </Card>
+        <Spacer>
+          <Text h3 style={styles.center}> Appointment Date</Text>
+        </Spacer>
+        <Spacer>
+          <Text style={styles.center}>{day} at {time}</Text>
+        </Spacer>
+        <Divider />
+        <Spacer>
+          <Text h3 style={styles.center}>Payment Information</Text>
+          <Button
+            title="Add a New Card"
+            onPress={() => navigation.navigate('Card')}
+          />
+        </Spacer>
+        <Divider />
+        <Spacer>
+          <Text h3 style={styles.center}>Addresses</Text>
+          <Spacer>
+            <FlatList
+              data={addresses}
+              keyExtractor={item => item._id}
+              renderItem={({ item }) => {
+                return (
+                  <TouchableOpacity>
+                    <ListItem
+                      key={item.item_id}
+                      bottomDivider>
+                      <ListItem.Content>
+                        <ListItem.Title>
+                          {item.line1} {item.line}
+                        </ListItem.Title>
+                        <ListItem.Subtitle>
+                          {item.city}, {item.state} {item.zipcode}
+                        </ListItem.Subtitle>
+                      </ListItem.Content>
+                    </ListItem>
+                  </TouchableOpacity>
+                )
+              }}
+            >
+            </FlatList>
+          </Spacer>
+          <Button
+            title="Add a New Address"
+            onPress={() => {
+              navigation.navigate('Address')
+            }}
+          />
+        </Spacer>
+        <Divider />
+        <Spacer>
+          <Button title="Confirm" />
+        </Spacer>
+      </ScrollView>
     </>
   )
 };
