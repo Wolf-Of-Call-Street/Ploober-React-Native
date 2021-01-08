57ae26f9bafac02e28d7b6ee9e956779514885db
Bramble <janem.draws@gmail.com>
2021-01-08 10:18:29 -0700

    Started on implementing Context state

--- client/src/components/Issue.js
+++ client/src/components/Issue.js
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import { View, Text } from 'react-native';
 import { Input } from 'react-native-elements';
+import { Context as AppointmentContext } from '../context/AppointmentContext';
 
 const Issue = ({onSubmit}) => {
   const [issue, setIssue] = useState('');
+  const {state} = useContext(AppointmentContext);
 
   return (
     <View>
@@ -12,8 +14,8 @@ const Issue = ({onSubmit}) => {
         label="Issue"
         value={issue}
         onChangeText={setIssue}
-        autoCorrect={false}
       />
+      {console.log(state.appointmentReason)}
     </View>
   )
 };
